d720d648bd389951740a532fff3c17d1ef19ccbe
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-21 23:45:38 -0700

    Update text_file_replace_all.py
    changed i to word

--- text_file_replace_all.py
+++ text_file_replace_all.py
@@ -2,7 +2,8 @@ import re
 
 word_swap = re.compile('story')
 
+#opening got.txt and creating got2.txt with swapped text
 with open('got.txt', 'r') as got:
   with open('got2.txt', 'w') as got2:
-    for i in got:
-      got2.write(word_swap.sub('fluffy',i))
+    for word in got:
+      got2.write(word_swap.sub('fluffy',word))
