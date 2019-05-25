cf25b70438ad94c1326f4bf461e46467b824f62f
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-25 13:10:35 -0700

    Update text_file_replace_all.py
    capitalized C in Creates and removed space between , 'r' and , 'w'

--- text_file_replace_all.py
+++ text_file_replace_all.py
@@ -1,11 +1,11 @@
-#text_file_replace_all.py: creates edited file with a swapped word
+#text_file_replace_all.py: Creates edited file with a swapped word
 
 import re
 
 word_swap = re.compile('story')
 
 #opening got.txt and creating got2.txt with swapped text
-with open('got.txt', 'r') as got:
-  with open('got2.txt', 'w') as got2:
+with open('got.txt','r') as got:
+  with open('got2.txt','w') as got2:
     for word in got:
       got2.write(word_swap.sub('fluffy',word))
