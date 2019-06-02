053193eaaeb142395b0f83e7804aaaa7fd8c1500
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-06-02 03:42:17 -0700

    Update text_file_replace_all.py
    reduced code with list compression in opening and creating files

--- text_file_replace_all.py
+++ text_file_replace_all.py
@@ -5,7 +5,5 @@ import re
 word_swap = re.compile('story')
 
 #opening got.txt and creating got2.txt with swapped text
-with open('got.txt','r') as got:
-  with open('got2.txt','w') as got2:
-    for word in got:
-      got2.write(word_swap.sub('fluffy',word))
+with open('got.txt','r') as got,open('got2.txt','w') as got2:
+    [got2.write(word_swap.sub('fluffy',word)) for word in got]
